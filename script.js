7b86ca22603c2e7d89ab5b06ce0f5c0f44ae6715
riveryoung100 <riveryoung100@gmail.com>
2026-07-08 20:30:31 -0500

    Rebranded website around personal platform

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ form.addEventListener("submit", function(event) {
     fetch("https://script.google.com/macros/s/AKfycbxnemMCr0vNCSMC4pQQXFtk_zFKVcY7S3DM6kr-1FoKsP-yIb3U1lmAlhIXi-qNybMv/exec", {
         method: "POST",
         body: formData
+    })
+    .then(() => {
+        alert("Thanks! You're connected. I'll be in touch soon.");
+        form.reset();
+    })
+    .catch(() => {
+        alert("Something went wrong. Please try again.");
     });
-
-    alert("Thanks! I received your information. I'll be in touch soon.");
-    form.reset();
 });
