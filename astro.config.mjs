82d4f9298aa044882e3245bf8840aa2c8c45b148
riveryoung100 <riveryoung100@gmail.com>
2026-07-29 12:02:40 -0500

    Recover homepage and restore Astro project

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+﻿import { defineConfig } from "astro/config";
+
+export default defineConfig({
+    output: "static",
+
+    build: {
+        assets: "_assets"
+    },
+
+    vite: {
+        build: {
+            sourcemap: false
+        }
+    }
+});
