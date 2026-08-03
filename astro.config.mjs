5b1239ccd9541dd567c8510c44123bd3bbcbb8d4
riveryoung100 <riveryoung100@gmail.com>
2026-08-03 09:48:38 -0500

    Add automatic sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
     site: "https://theriverkeptflowing.com",
     output: "static",
 
     integrations: [
-        mdx()
+        mdx(),
+        sitemap()
     ],
 
     build: {
