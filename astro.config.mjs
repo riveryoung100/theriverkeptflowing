1235d474a716770b44e164964177a41676aac1b5
riveryoung100 <riveryoung100@gmail.com>
2026-09-06 17:05:05 -0500

    PRODUCT-001E-03: Establish Cloudflare runtime foundation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
     site: "https://theriverkeptflowing.com",
     output: "static",
+
+    adapter: cloudflare({
+        imageService: "passthrough",
+        prerenderEnvironment: "node"
+    }),
 
     integrations: [
         mdx(),
